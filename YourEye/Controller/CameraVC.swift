36128d835b94b903b14773711bdc8d4ad361050d
Elshakoushy <melshakoushy@hotmail.com>
2019-06-26 16:07:49 +0200

    first

---
+++ YourEye/Controller/CameraVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YourEye
+//
+//  Created by Mahmoud Elshakoushy on 6/26/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
